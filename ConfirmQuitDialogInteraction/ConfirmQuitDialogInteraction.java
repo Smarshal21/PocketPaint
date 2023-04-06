61a85acf723d3d114fc66314eaa40d1d284bbc3e
Smarshall <99678760+Smarshal21@users.noreply.github.com>
2023-04-06 18:17:18 +0530

    Update ConfirmQuitDialogInteraction.java

--- ConfirmQuitDialogInteraction/ConfirmQuitDialogInteraction.java
+++ ConfirmQuitDialogInteraction/ConfirmQuitDialogInteraction.java
@@ -27,8 +27,6 @@ import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.ViewInteraction;
 
 import static org.hamcrest.Matchers.allOf;
-
-import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 

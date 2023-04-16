2542f497cf433a3a238da1c5c6bc33ab23687375
Smarshall <99678760+Smarshal21@users.noreply.github.com>
2023-04-17 00:48:54 +0530

    Update ConfirmQuitDialogInteraction.kt

--- ConfirmQuitDialogInteraction/ConfirmQuitDialogInteraction.kt
+++ ConfirmQuitDialogInteraction/ConfirmQuitDialogInteraction.kt
@@ -1,17 +1,4 @@
 
-    
-
-    fun checkMessage(matcher: ViewAssertion?): ConfirmQuitDialogInteraction {
-        Espresso.onView(ViewMatchers.withText(R.string.closing_security_question))
-            .check(matcher)
-        return this
-    }
-
-    fun checkTitle(matcher: ViewAssertion?): ConfirmQuitDialogInteraction {
-        Espresso.onView(ViewMatchers.withText(R.string.closing_security_question_title))
-            .check(matcher)
-        return this
-    }
 
     companion object {
         @JvmStatic
